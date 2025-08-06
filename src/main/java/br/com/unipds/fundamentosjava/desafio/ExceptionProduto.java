35f24b3e9e32553cad22c154c31847a6f742c224
AlexOlieira-Ipira <alex.borgesoliveira@gmail.com>
2025-08-06 01:12:39 -0300

    Retorno Desafio Encapsulamento

---
+++ src/main/java/br/com/unipds/fundamentosjava/desafio/ExceptionProduto.java
@@ -0,0 +1,7 @@
+package br.com.unipds.fundamentosjava.desafio;
+
+public class ExceptionProduto extends RuntimeException{
+    public ExceptionProduto(String msg){
+        super(msg);
+    }
+}
--- src/main/java/br/com/unipds/fundamentosjava/desafio/Produto.java
+++ src/main/java/br/com/unipds/fundamentosjava/desafio/Produto.java
@@ -2,16 +2,52 @@ package br.com.unipds.fundamentosjava.desafio;
 
 public class Produto {
 
-    public int id;
-    public String nome;
-    public double preco;
-    public int quantidadeEmEstoque;
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
 
-    public Produto(int id, String nome, double preco, int quantidadeEmEstoque) {
+    public Produto(int id, String nome, double preco, int quantidadeEmEstoque)  {
         this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        if(nome != null && !nome.isBlank()){
+            this.nome = nome;
+        }else{
+            throw new ExceptionProduto("Nome do produto " + id + " não pode ser nulo ou vazio" );
+        }
+        if(preco > 0){
+            this.preco = preco;
+        }else{
+            throw new ExceptionProduto("Preço do produto " + id + " não pode ser negativo");
+        }
+        if(quantidadeEmEstoque > 0){
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }else {
+            throw new ExceptionProduto("Quantidade do produto " + id + " não pode ter valor negativo ou zero");
+        }
+    }
+
+    public void setPreco(double preco) {
+        if(preco > 0){
+            this.preco = preco;
+        }else{
+            throw new ExceptionProduto("Preço do produto " + id + " não pode ser negativo");
+        }
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        if(quantidadeEmEstoque > 0){
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }else {
+            throw new ExceptionProduto("Quantidade do produto " + id + " não pode ter valor negativo ou zero");
+        }
+    }
+
+    public void setNome(String nome) {
+        if(nome != null && !nome.isBlank()){
+            this.nome = nome;
+        }else{
+            throw new ExceptionProduto("Nome do produto " + id + " não pode ser nulo ou vazio" );
+        }
     }
 
     @Override
